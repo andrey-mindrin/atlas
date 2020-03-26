8ce73a47e3cdaf3abf3cda84732475404daed60b
Sergey Chernolyas <sergey.chernolyas@gmail.com>
2020-03-26 11:19:17 +0300

    add logs

--- repository/src/main/java/org/apache/atlas/discovery/HdfsAtlasDiscoveryService.java
+++ repository/src/main/java/org/apache/atlas/discovery/HdfsAtlasDiscoveryService.java
@@ -1,16 +1,20 @@
 package org.apache.atlas.discovery;
 
+import java.util.HashSet;
 import org.apache.atlas.annotation.GraphTransaction;
 import org.apache.atlas.exception.AtlasBaseException;
 import org.apache.atlas.model.discovery.AtlasSearchResult;
 import org.apache.atlas.model.discovery.SearchParameters;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
 
 @Component
 public class HdfsAtlasDiscoveryService {
+    private static final Logger LOG  = LoggerFactory.getLogger(HdfsAtlasDiscoveryService.class);
     private AtlasDiscoveryService discoveryService;
 
     @Inject
@@ -27,7 +31,10 @@ public class HdfsAtlasDiscoveryService {
         parameters.setIncludeSubTypes(true);
         parameters.setLimit(25);
         parameters.setOffset(0);
+        parameters.setAttributes(new HashSet<>());
         parameters.setTypeName("hive_storagedesc");
+        LOG.info("QUERY :"+parameters.getQuery());
+        LOG.info("SearchParameters :"+parameters.toString());
 
         return discoveryService.searchWithParameters(parameters);
     }
